9e220c5182cf6cfde917bccfc5171189f6e98953
Busarang <riyukiara@gmail.com>
2025-02-11 22:49:25 +0800

    Backend setup
    Setting up the backend with spring boot

---
+++ backend/edu.cit.lemslite/src/main/java/edu/cit/lemslite/LemsliteApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.lemslite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemsliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemsliteApplication.class, args);
+	}
+
+}
