f17f4836eb725eda086f2fad96ef1aab840dc898
Mark Bandola <143799613+MarkJanzenB@users.noreply.github.com>
2025-04-09 17:35:52 +0800

    added Manufacturer/subject/yearsection entities, controllers, repository, and service

---
+++ backend/src/main/java/edu/cit/lemslite/Controller/SubjectController.java
@@ -0,0 +1,33 @@
+package edu.cit.lemslite.Controller;
+
+import edu.cit.lemslite.Entity.SubjectEntity;
+import edu.cit.lemslite.Service.SubjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subject")
+public class SubjectController {
+
+    @Autowired
+    SubjectService sserv;
+
+    @PostMapping("/addsubject")
+    public SubjectEntity addSubject(@RequestBody SubjectEntity subject){
+     return sserv.addSubject(subject);
+    }
+    @GetMapping("/getallsubject")
+    public List<SubjectEntity> getAllSubject(){
+        return sserv.getAllSubject();
+    }
+//    @PutMapping("/updatesubject/{subject_id}")
+//    public SubjectEntity updateSubject(@RequestBody SubjectEntity subject, @PathVariable int subject_id){
+//        return sserv.updateSubject(subject,subject_id);
+//    }
+    @DeleteMapping("/deleteSubject/{subject_id}")
+    public void deleteSubject(@PathVariable int subject_id){
+        sserv.deleteSubject(subject_id);
+    }
+}
--- backend/src/main/java/edu/cit/lemslite/Entity/TeacherScheduleEntity.java
+++ backend/src/main/java/edu/cit/lemslite/Entity/TeacherScheduleEntity.java
@@ -9,7 +9,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.sql.Time;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 
@@ -17,7 +23,7 @@ import java.util.Date;
 
 //to be revised
 @Entity
-@Table(name = "TeacherSchedule")
+@Table(name = "teacher_schedule")
 public class TeacherScheduleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,17 +35,60 @@ public class TeacherScheduleEntity {
     @Column(name = "end_time")
     private Time endTime;
     @Column(name="lab_num")
-    private int labNum;
+    private String labNum;
     @Column(name = "date")
     //Date default format is YYYY-MM-DD
     private Date date;
-    @Column(name = "teacher_id")
-    private int teacherId;
+    //    @Column(name = "teacher_id")
+//    private int teacherId;
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", nullable = true)
+    private UserEntity teacher;
+    @ManyToOne
+    @JoinColumn(name = "year_id", nullable = true)
+    private YearSectionEntity yearSection;
+    @ManyToOne
+    @JoinColumn(name = "createdby_id", nullable = true)
+    private UserEntity createdBy;
+
+    @Column(name = "date_created")
+    private LocalDate dateCreated;
+
+
+    @OneToOne(mappedBy = "teacherSchedule")
+    private BorrowItemEntity BorrowItemEntity;
+
+    //Add sched_assign_id FK
+    //Add subject_id FK
+
     public TeacherScheduleEntity() {
         super();
     }
 
-	@JsonProperty("teacher_schedule_id")
+
+
+    public TeacherScheduleEntity(int teacherScheduleId, Time startTime, Time endTime, String labNum, Date date,
+                                 UserEntity teacher, YearSectionEntity yearSection, UserEntity createdBy, LocalDate dateCreated) {
+        super();
+        this.teacherScheduleId = teacherScheduleId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.labNum = labNum;
+        this.date = date;
+        this.teacher = teacher;
+        this.yearSection = yearSection;
+        this.createdBy = createdBy;
+        this.dateCreated = dateCreated;
+    }
+
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.dateCreated = LocalDate.now();
+    }
+
+    @JsonProperty("teacher_schedule_id")
     public int getTeacherScheduleId() {
         return teacherScheduleId;
     }
@@ -61,10 +110,10 @@ public class TeacherScheduleEntity {
         this.endTime = endTime;
     }
     @JsonProperty("lab_num")
-    public int getLabNum() {
+    public String getLabNum() {
         return labNum;
     }
-    public void setLabNum(int labNum) {
+    public void setLabNum(String labNum) {
         this.labNum = labNum;
     }
     @JsonProperty("date")
@@ -74,11 +123,38 @@ public class TeacherScheduleEntity {
     public void setDate(Date date) {
         this.date = date;
     }
-    @JsonProperty("teacher_id")
-    public int getTeacherId() {
-        return teacherId;
+
+    @JsonProperty("teacher")
+    public UserEntity getTeacher() {
+        return teacher;
     }
-    public void setTeacherId(int teacherId) {
-        this.teacherId = teacherId;
+
+    public void setTeacher(UserEntity teacher) {
+        this.teacher = teacher;}
+
+    @JsonProperty("year_and_section")
+    public YearSectionEntity getYearSection() {
+        return yearSection;
+    }
+    @JsonProperty("created_by")
+    public UserEntity getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(UserEntity createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    @JsonProperty("date_created")
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public void setYearSection(YearSectionEntity yearSection) {
+        this.yearSection = yearSection;
     }
 }
