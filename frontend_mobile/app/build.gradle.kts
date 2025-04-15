76e2342a2cb25fabfb5e3c924aa00d80702d7cfc
kingkuys2123 <kylematthew.quitco@cit.edu>
2025-04-15 17:51:22 +0800

    Implemented the login page (Static)

--- frontend_mobile/app/build.gradle.kts
+++ frontend_mobile/app/build.gradle.kts
@@ -39,10 +39,9 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.material)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.annotation)
