8e59a676b6ebaf31a925b91cdc04d2d9450a7b5e
kingkuys2123 <kylematthew.quitco@cit.edu>
2025-04-27 06:37:15 +0800

    Mobile Frontend: Implemented register functionality (working)

--- frontend_mobile/app/src/main/java/com/example/lemslite/RegisterSuccessActivity.kt
+++ frontend_mobile/app/src/main/java/com/example/lemslite/RegisterSuccessActivity.kt
@@ -1,20 +1,32 @@
 package com.example.lemslite
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class RegisterSuccessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_register_success)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val continueButton: MaterialButton? = findViewById(R.id.btn_continue)
+        continueButton?.setOnClickListener {
+            navigateToLogin()
+        }
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
