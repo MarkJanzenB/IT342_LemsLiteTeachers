cc24481b4cc41443f03db1f5a800962c4459f9a9
kingkuys2123 <kylematthew.quitco@cit.edu>
2025-04-29 17:42:55 +0800

    Mobile Frontend: Display Full name in Edit Full Name Page

--- frontend_mobile/app/src/main/java/com/example/lemslite/EditFullNameActivity.kt
+++ frontend_mobile/app/src/main/java/com/example/lemslite/EditFullNameActivity.kt
@@ -12,10 +12,22 @@ class EditFullNameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_edit_full_name)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val jwtService = JwtService()
+        val token = getSharedPreferences("user_session", MODE_PRIVATE).getString("jwt_token", null)
+
+        if (token != null) {
+            val firstName = jwtService.getFNameFromToken(token)
+            val lastName = jwtService.getLNameFromToken(token)
+
+            findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.firstNameEditText).setText(firstName)
+            findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.lastNameEditText).setText(lastName)
         }
 
         val backIcon = findViewById<ImageView>(R.id.backIcon)
