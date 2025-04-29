5e026216ec6a4ab7bca35e3b5df4231952cf9cfc
kingkuys2123 <kylematthew.quitco@cit.edu>
2025-04-29 11:23:56 +0800

    Mobile Frontend: Clicking the USer Profile icon will direct the user to the Account Page

--- frontend_mobile/app/src/main/java/com/example/lemslite/HomeActivity.kt
+++ frontend_mobile/app/src/main/java/com/example/lemslite/HomeActivity.kt
@@ -1,20 +1,29 @@
 package com.example.lemslite
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ImageView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val profileIcon = findViewById<ImageView>(R.id.profileIcon)
+        profileIcon.setOnClickListener {
+            val intent = Intent(this, AccountActivity::class.java)
+            startActivity(intent)
         }
     }
 }
