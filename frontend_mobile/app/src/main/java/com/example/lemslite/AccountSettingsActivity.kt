c243963a68893bdd222b845b4c71fd14cee66a07
kingkuys2123 <kylematthew.quitco@cit.edu>
2025-04-29 11:27:45 +0800

    Mobile Frontend: backIcon now functional

--- frontend_mobile/app/src/main/java/com/example/lemslite/AccountSettingsActivity.kt
+++ frontend_mobile/app/src/main/java/com/example/lemslite/AccountSettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lemslite
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,11 @@ class AccountSettingsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backIcon = findViewById<ImageView>(R.id.backIcon)
+        backIcon.setOnClickListener {
+            finish()
         }
     }
 }
