36bb668e38a5ab8c487bac50ad31aa500c0d6ea7
kingkuys2123 <kylematthew.quitco@cit.edu>
2025-05-07 21:02:49 +0800

    Mobile Backend: Update BASE_URL for local API testing

--- frontend_mobile/app/src/main/java/com/example/lemslite/RetrofitInstance.kt
+++ frontend_mobile/app/src/main/java/com/example/lemslite/RetrofitInstance.kt
@@ -10,10 +10,10 @@ import okhttp3.logging.HttpLoggingInterceptor
 object RetrofitInstance {
 
     // Cloud
-    private const val BASE_URL = "https://it342-lemsliteteachers.onrender.com/"
+    // private const val BASE_URL = "https://it342-lemsliteteachers.onrender.com/"
 
     // Local (For API testing)
-    // private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     private val gson: Gson = GsonBuilder()
         .setLenient()
